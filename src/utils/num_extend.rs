9de72cefed941f7eaa342f39e804c37631346677
XNTEABDSC <2364400855@qq.com>
2026-05-11 12:56:13 +0800

    .

--- src/utils/num_extend.rs
+++ src/utils/num_extend.rs
@@ -1,15 +1,18 @@
 use num_traits::Num;
 
-pub trait NumExtend:Num{
+pub trait NumExtends:Num{
 	fn p2()->Self{
 		Self::one()+Self::one()
 	}
 	fn p3()->Self{
 		Self::one()+Self::one()+Self::one()
 	}
+	fn frac_1_2()->Self{
+		Self::one()/(Self::one()+Self::one())
+	}
 	
 }
 
-impl<T:Num> NumExtend for T{
+impl<T:Num> NumExtends for T{
 
 }
